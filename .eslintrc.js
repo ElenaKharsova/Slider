6b9d98ac710d551e8143653234bb3ab8f3f2ccc2
Elena Kharsova <elena.kharsova@gmail.com>
2023-05-12 01:05:29 +0200

    lintering

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['jest'],
+  plugins: ["jest"],
   rules: {
-    'max-len': [
-      'error',
+    quotes: [2, "double", { avoidEscape: true }],
+    "comma-dangle": "off",
+    "max-len": [
+      "error",
       {
         ignoreComments: true,
       },
     ],
-    'import/prefer-default-export': 'off',
+    "import/prefer-default-export": "off",
+    "no-plusplus": "off",
+    "no-unused-expressions": "off",
+    "func-names": "off",
+    "no-console": "off",
+    "no-param-reassign": "off",
   },
 };
